94607eb42987027f635db3e72ff104882e8a368b
Andrew Radamis <andrewradamis@paay.co>
2022-02-25 10:46:31 -0500

    Remove Template Dependency

--- aws_sns_topic.tf
+++ aws_sns_topic.tf
@@ -1,12 +1,12 @@
 // SNS topic
 
-data "template_file" "aws_sns_topic_policy" {
-  template = file("${path.module}/aws_sns_topic_policy.tpl")
+locals {
+  aws_sns_topic_policy = file("${path.module}/aws_sns_topic_policy.tpl")
 }
 
 resource "aws_sns_topic" "sns" {
-  name         = var.aws_optional_conf.sns_topic_name
-  tags         = var.aws_optional_conf.tags
+  name = var.aws_optional_conf.sns_topic_name
+  tags = var.aws_optional_conf.tags
 
   display_name = var.aws_optional_conf.sns_topic_display_name
   depends_on   = [aws_iam_role.role]
@@ -14,6 +14,6 @@ resource "aws_sns_topic" "sns" {
 
 resource "aws_sns_topic_policy" "sns" {
   arn    = aws_sns_topic.sns.arn
-  policy = data.template_file.aws_sns_topic_policy.rendered
+  policy = local.aws_sns_topic_policy
 }
 
--- aws_sqs_queue.tf
+++ aws_sqs_queue.tf
@@ -1,22 +1,22 @@
 // Setup SQS
 
-data "template_file" "aws_sqs_queue_policy" {
-  template = file("${path.module}/aws_sqs_queue_policy.tpl")
-  vars = {
+locals {
+  aws_sqs_queue_policy_vars = {
     sns_arn = aws_sns_topic.sns.arn
   }
+  aws_sqs_queue_policy = templatefile("${path.module}/aws_sqs_queue_policy.tpl", local.aws_sqs_queue_policy_vars)
 }
 
 resource "aws_sqs_queue" "sqs" {
-  name       = var.aws_optional_conf.sqs_queue_name
-  tags       = var.aws_optional_conf.tags
+  name = var.aws_optional_conf.sqs_queue_name
+  tags = var.aws_optional_conf.tags
 
   depends_on = [aws_sns_topic_policy.sns]
 }
 
 resource "aws_sqs_queue_policy" "sqs" {
   queue_url = aws_sqs_queue.sqs.id
-  policy    = data.template_file.aws_sqs_queue_policy.rendered
+  policy    = local.aws_sqs_queue_policy
 }
 
 resource "aws_sns_topic_subscription" "sqs" {
